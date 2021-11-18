69aa5bf3a1297533ea1dd223275d341c25ba90fb
beadx6ggwp <davidmd9830415@gmail.com>
2021-11-19 00:36:36 +0800

    Update main.cpp

--- Problems/[x][E]448. Find All Numbers Disappeared in an Array/main.cpp
+++ Problems/[x][E]448. Find All Numbers Disappeared in an Array/main.cpp
@@ -1,15 +1,28 @@
 #include <bits/stdc++.h>
+
 #include <unordered_set>
 using namespace std;
 
 class Solution {
    public:
+    // vector<int> findDisappearedNumbers(vector<int>& nums) {
+    //     unordered_set<int> s(begin(nums), end(nums));  // insert every
+    //     nums[i] in hashset vector<int> ans(nums.size() - s.size()); for (int
+    //     i = 1, j = 0; i <= nums.size(); i++)
+    //         if (!s.count(i))
+    //             ans[j++] = i;  // add all elements not found in hashset to
+    //             ans
+    //     return ans;
+    // }
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        unordered_set<int> s(begin(nums), end(nums));  // insert every nums[i] in hashset
-        vector<int> ans(nums.size() - s.size());
-        for (int i = 1, j = 0; i <= nums.size(); i++)
-            if (!s.count(i))
-                ans[j++] = i;  // add all elements not found in hashset to ans
+        // time:O(n), spzec:O(1)
+        vector<int> ans;
+        for (auto c : nums)
+            nums[abs(c) - 1] = -abs(
+                nums[abs(c) - 1]);  // mark c is present by negating nums[c-1]
+        for (int i = 0; i < size(nums); i++)
+            if (nums[i] > 0) ans.push_back(i + 1);
+        // nums[i] > 0 means i+1 isnt present in nums
         return ans;
     }
 };
