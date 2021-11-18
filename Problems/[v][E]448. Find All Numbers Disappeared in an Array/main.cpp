3b020aa453ef98b7c7a9ab65ea46af7338608933
beadx6ggwp <davidmd9830415@gmail.com>
2021-11-19 07:19:38 +0800

    update 448

--- Problems/[v][E]448. Find All Numbers Disappeared in an Array/main.cpp
+++ Problems/[v][E]448. Find All Numbers Disappeared in an Array/main.cpp
@@ -14,15 +14,30 @@ class Solution {
     //             ans
     //     return ans;
     // }
+    // vector<int> findDisappearedNumbers(vector<int>& nums) {
+    //     // time:O(n), spzec:O(1)
+    //     vector<int> ans;
+    //     for (auto c : nums)
+    //         nums[abs(c) - 1] = -abs(
+    //             nums[abs(c) - 1]);  // mark c is present by negating
+    //             nums[c-1]
+    //     for (int i = 0; i < size(nums); i++)
+    //         if (nums[i] > 0) ans.push_back(i + 1);
+    //     // nums[i] > 0 means i+1 isnt present in nums
+    //     return ans;
+    // }
+
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        // time:O(n), spzec:O(1)
+        // 想法: 第一輪走訪把nums[i]換到第i格
+        // 然後第二輪的時候去按nums[i] 跟 i 是不是對應的
         vector<int> ans;
-        for (auto c : nums)
-            nums[abs(c) - 1] = -abs(
-                nums[abs(c) - 1]);  // mark c is present by negating nums[c-1]
-        for (int i = 0; i < size(nums); i++)
-            if (nums[i] > 0) ans.push_back(i + 1);
-        // nums[i] > 0 means i+1 isnt present in nums
+        for (auto& c : nums)
+            while (nums[c - 1] != c)
+                swap(c, nums[c - 1]);  // swap till correct index place of c is
+                                       // not occupied by c itself
+        for (int i = 1; i <= nums.size(); i++)
+            if (i != nums[i - 1]) ans.push_back(i);
+        // correct index place of i is not occupied by i itself
         return ans;
     }
 };
