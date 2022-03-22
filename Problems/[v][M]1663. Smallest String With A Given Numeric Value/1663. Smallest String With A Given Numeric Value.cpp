757c70275d2c1e07f6d56fcb1998450ad11406ad
beadx6ggwp <davidmd9830415@gmail.com>
2022-03-22 09:46:27 +0800

    Update 1663. Smallest String With A Given Numeric Value.cpp

--- Problems/[v][M]1663. Smallest String With A Given Numeric Value/1663. Smallest String With A Given Numeric Value.cpp
+++ Problems/[v][M]1663. Smallest String With A Given Numeric Value/1663. Smallest String With A Given Numeric Value.cpp
@@ -1,13 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-public:
+   public:
     string getSmallestString(int n, int k) {
-        
+        string res(n, 'a');
+        k -= n;
+        int last = res.size() - 1;
+        while (k >= 0 && last >= 0) {
+            if (k >= 26) {
+                res[last--] = 'z';
+            } else {
+                res[last--] = (char)(k + 'a');
+            }
+            k -= 25;
+        }
+
+        return res;
     }
 };
 int main() {
     cout << "[1663. Smallest String With A Given Numeric Value]\n-----\n";
-    
+    // cout << Solution().getSmallestString(3, 27) << "\n";
+    cout << Solution().getSmallestString(5, 130) << "\n";
     return 0;
 }
